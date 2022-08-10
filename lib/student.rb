839d562f207fbdd3356c20a13496fb0a7f4b8c39
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-10 15:18:30 -0700

    commit tree 193046c1cf819eb40a10632690a16ae5f4976c0e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    # if you want to make it a special type of variable, initialize
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        # put/migrate v into @knowledge (.push)
+        @knowledge << knowledge
+    end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,4 @@
+# define user class with first & last name (setter & getter)
+class User
+    attr_accessor :first_name, :last_name
+end
